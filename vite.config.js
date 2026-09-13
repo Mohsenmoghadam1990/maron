1ffc68dbd2074e0b7349fc8393f6db58c594a422
mohsenmoghadam <mohsenmoghadam1990@yahoo.com>
2026-09-13 19:11:00 +0330

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/kohan-tejarat/",
+
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
   ],
-})
+});
